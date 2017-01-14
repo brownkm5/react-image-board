b1b5e7a1682a2eed66b0c35d8be66d9c8b196af7
Kevin Brown <brownkm5@g.cofc.edu>
2017-01-14 09:50:02 -0500

    working

---
+++ app/scripts/components/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var AppComponent = React.createClass({
+  render: function(){
+    return (
+      <h1>working</h1>
+    )
+  }
+});
+
+module.exports = {
+  AppComponent: AppComponent
+}
